4d30fbb216be3f1fc46ed6150d8947485dbff5bb
Arpan <164992371+realarpan@users.noreply.github.com>
2026-04-03 18:27:12 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://ai-chat-backend-5ha6.onrender.com/" });
 API.interceptors.request.use((req)=>{
   const token = localStorage.getItem("token");
   if(token) req.headers.Authorization = token;
