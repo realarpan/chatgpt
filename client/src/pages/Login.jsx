6103d98ecfa6b29c97ca4f41b4a3c5b94b7be34f
Arpan <164992371+realarpan@users.noreply.github.com>
2026-04-03 17:45:12 +0530

    Create Login.jsx

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import API from "../api";
+import { useNavigate } from "react-router-dom";
+
+export default function Login(){
+  const [email,setEmail]=useState("");
+  const [password,setPassword]=useState("");
+  const navigate = useNavigate();
+
+  const login = async()=>{
+    const res = await API.post("/auth/login",{email,password});
+    localStorage.setItem("token",res.data.token);
+    navigate("/chat");
+  }
+
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <div className="p-6 shadow rounded">
+        <input placeholder="Email" onChange={e=>setEmail(e.target.value)}/>
+        <input type="password" placeholder="Password" onChange={e=>setPassword(e.target.value)}/>
+        <button onClick={login}>Login</button>
+      </div>
+    </div>
+  );
+}
