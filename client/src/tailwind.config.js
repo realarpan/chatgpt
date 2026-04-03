7e0016bb9df1d4f4386d73f78788a66fd920a267
Arpan <164992371+realarpan@users.noreply.github.com>
2026-04-03 21:15:27 +0530

    Update tailwind.config.js

--- client/src/tailwind.config.js
+++ client/src/tailwind.config.js
@@ -1,11 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx}"
-  ],
-  theme: {
-    extend: {},
-  },
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: { extend: {} },
   plugins: [],
 };
